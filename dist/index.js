9256bdc416be3d3a6fcbb3e0fb9edc0208cec9f1
cloundfile <tecnologiaesolucoespr@gmail.com>
2025-03-04 09:45:56 -0300

    fix: cors

--- dist/index.js
+++ dist/index.js
@@ -10,18 +10,29 @@ var cors = require('cors');
 data_source_1.AppDataSource.initialize().then(() => {
     const app = (0, express_1.default)();
     app.use(express_1.default.json());
+    /*
+     HttpServletRequest request = (HttpServletRequest) req;
+          HttpServletResponse response = (HttpServletResponse) res;
+          response.setHeader("Access-Control-Allow-Origin",  request.getHeader("Origin"));
+          response.setHeader("Access-Control-Allow-Credentials", "true");
+          response.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,OPTIONS");
+          response.setHeader("Access-Control-Max-Age", "3600");
+          response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me, Authorization, type ");
+          response.setHeader("Access-Control-Expose-Headers","Authorization");
+          */
     app.use((req, res, next) => {
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+        res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,OPTIONS");
+        res.setHeader("Access-Control-Max-Age", "3600");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me, Authorization, type ");
+        res.setHeader("Access-Control-Expose-Headers", "Authorization");
         if (req.method === 'OPTIONS') {
-            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
             return res.status(200).send({});
         }
         next();
     });
-    app.use(cors({
-        allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization']
-    }));
+    app.use(cors());
     app.use(routes_1.default);
     return app.listen(process.env.PORT || 3000);
 }).catch(() => {
